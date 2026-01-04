566bee638236d59ec7bf1b10fae40471c539317e
baclobas <7378947+alkus@users.noreply.github.com>
2026-01-04 11:00:42 +0000

    Episode2 code

---
+++ Ep002/source/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <concepts>
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b) {
+  return a + b;
+}
+
+int main() {
+  std::cout << "Hello from C++20 with CMake on Linux/Windows" << '\n';
+  std::cout << "The sum is : " << add(7,3) << '\n';
+}
